da045bb409f88c77beaf79f476318ef14a6118ac
William Schueller <william.schueller@gmail.com>
2018-08-15 19:00:52 +0200

    update

--- configs/cfg_func.py
+++ configs/cfg_func.py
@@ -20,7 +20,7 @@
             "env_cfg": {
               "env_type": "simple",
               "M": {{% M,20 %}}, #number of meanings
-              "W": 500  #number of words
+              "W": {{% M,20 %}}  #number of words
             },
             "interact_cfg": {
               "interact_type": "speakerschoice"
@@ -29,7 +29,7 @@
         }
     if {{% active,True %}}:
         base_cfg['pop_cfg']['strat_cfg'].update(**{
-                'strat_type':'lapsmax_mab_explothreshold','bandit_type':'bandit_laps','gamma':{{% gamma,1. %}},'time_scale':{{% time_scale,2 %}},
+                'strat_type':'lapsmax_mab_explothreshold','bandit_type':'bandit_laps','gamma':{{% gamma,0.1 %}},'time_scale':{{% time_scale,2 %}},
                 "memory_policies": [{
                       "time_scale": {{% time_scale,2 %}},
                       "mem_type": "interaction_counts_sliding_window_local"
@@ -66,5 +66,52 @@
         }
     if {{% active,True %}}:
         base_cfg['pop_cfg']['strat_cfg'].update(**{
-                'strat_type':'coherence','time_scale':{{% time_scale,2 %}},, })
+                'strat_type':'coherence','time_scale':{{% time_scale,2 %}}, 
+                "memory_policies": [{
+                      "time_scale": {{% time_scale,2 %}},
+                      "mem_type": "interaction_counts_sliding_window_local"
+                        }],})
+    return base_cfg
+
+###halfline###
+
+    base_cfg = {
+          "step": "log_improved",
+          "pop_cfg": {
+            "voc_cfg": {
+          "voc_type": 'matrix_new'
+            },
+        "strat_cfg": {
+          "vu_cfg": {
+            "vu_type": "minimal"
+          },
+          "success_cfg": {
+            "success_type": "global_norandom"
+              },
+              "strat_type": "naive" #random topic choice: changed if active is True
+            },
+            "nbagent": {{% N,10 %}}, #population size
+            "env_cfg": {
+              "env_type": "simple",
+              "M": {{% M,20 %}}, #number of meanings
+              "W": {{% W,100 %}}  #number of words
+            },
+            "interact_cfg": {
+              "interact_type": "speakerschoice"
+            },
+            "topology_cfg": {
+              'topology_type':'line'
+            },
+            "agent_init_cfg": {
+              'agent_init_type':'converged_halfline'
+            },
+          }
+        }
+    if {{% active,True %}}:
+        base_cfg['pop_cfg']['strat_cfg'].update(**{
+                'strat_type':'coherence','time_scale':{{% time_scale,2 %}}, 
+                "memory_policies": [{
+                      "time_scale": {{% time_scale,2 %}},
+                      "mem_type": "interaction_counts_sliding_window_local"
+                        }],})
     return base_cfg
